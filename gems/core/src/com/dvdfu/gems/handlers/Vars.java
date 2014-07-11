f603cce1fba6e122742b69194dd5cc7e67d131ce
david <davidf1212@gmail.com>
2014-07-10 22:17:51 -0400

    Eclipse cleanup

---
+++ gems/core/src/com/dvdfu/gems/handlers/Vars.java
@@ -0,0 +1,20 @@
+package com.dvdfu.gems.handlers;
+
+public class Vars {
+	public static final boolean editor = false;
+	
+	public static final int screenWidth = 480;
+	public static final int screenHeight = 480;
+
+	public static final int ticksPerSecond = 60;
+	public static final int ticksPerFrame = 4;
+	public static final float framesPerSecond = ticksPerSecond / ticksPerFrame;
+
+	public static final int fullSize = 32;
+	public static final int halfSize = fullSize / 2;
+	public static final int timeMove = 4;
+	public static final int timePath = 16;
+	public static final int timeGem = 16;
+	public static final int timeDrown = 2;
+	public static final int timeExplode = 2;
+}
